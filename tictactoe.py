754aaf17f2de286be3b92c01a400c00a7b4727ca
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-05 08:09:18 -0400

    Updated from Brython Server: 05/04/2018 08:09:17

--- tictactoe.py
+++ tictactoe.py
@@ -7,23 +7,23 @@ from ggame import *
 
 
 def isEmpty(squarenum):         #returns True if the square "squarenum" is empty and False if there is already an X or O there.
-
+    return
 
 
 def winner():                   #returns True if someone won and False otherwise, but not who won
-
+    return
 
 
 def fullboard():                #returns True if every square is filled up with an X or O and False otherwise.
-
+    return
 
 
 def computerTurn():             #picks a random unused square and places the computer piece there.
-
+    return
 
 
 def mouseClick(event):
-    
+    return
 
 
 if __name__ == '__main__':
@@ -41,16 +41,22 @@ black = Color(0x000000,1)
 red = Color(0xFF0000,1)
 
 #setting up board
-vertBoardLine = RectangleAsset(width,height,LineStyle(1,black),black)
-horizBoardLine = RectangleAsset(height,width,LineStyle(1,black),black)
 
-def printBoard():            #prints out the board with the Xs and Os in the correct places and numbers in the open spots.
-    blackline = 
+HEIGHT = 300
+WIDTH = 300
 
-
-
+vertBoardLine = RectangleAsset(WIDTH,HEIGHT,LineStyle(1,black),black)
+horizBoardLine = RectangleAsset(HEIGHT,WIDTH,LineStyle(1,black),black)
 
 
+
+
+Sprite(horizBoardLine(200+HEIGHT/3,200))
+Sprite(horizBoardLine(200+(2*HEIGHT/3),200))
+Sprite(vertBoardLine(200,200+WIDTH/3))
+Sprite(vertBoardLine(200,200+(2*WIDTH/3)))
+
+App().run()
 
 
 
