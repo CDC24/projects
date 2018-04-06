69d55b4e7f8fc5c0fa0483ae9b0e9d5ce8889b33
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-06 08:14:00 -0400

    Updated from Brython Server: 06/04/2018 08:13:59

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,7 @@ from random import randint
 
 
 def isEmpty(squarenum):         #returns True if the square "squarenum" is empty and False if there is already an X or O there.
-    return
+    isEmpty()=True
 
 
 def winner():                   #returns True if someone won and False otherwise, but not who won
@@ -28,36 +28,45 @@ def computerTurn():             #picks a random unused square and places the com
     
     if horiz==1:                                        #decides for first column numbers
         
-        if vert == 1:
+        if vert == 1 and isEmpty(11)=True:
             Sprite (Os,(120,100))
+            isEmpty(11)=False
             
-        elif vert == 2:
+        elif vert == 2 and isEmpty(12)=True:
             Sprite (Os,(120,230))
+            isEmpty(12)=False
             
-        elif vert == 3:
+        elif vert == 3 and isEmpty(12)=True:
             Sprite (Os,(120,360))
+            isEmpty(13)=False
             
     elif horiz == 2:                                    #decides for second column numbers
         
-        if vert == 1:
+        if vert == 1 and isEmpty(21)=True:
             Sprite (Os,(250,100))
+            isEmpty(21)=False
             
-        elif vert == 2:
+        elif vert == 2 and isEmpty(22)=True:
             Sprite (Os,(250,230))
+            isEmpty(22)=False
             
-        elif vert == 3:
+        elif vert == 3 and isEmpty(23)=True:
             Sprite (Os,(250,360))
+            isEmpty(23)=False
             
     elif horiz == 3:                            #decides for third column numbers
         
-        if vert == 1:
+        if vert == 1 and isEmpty(31)=True:
             Sprite (Os,(380,100))
+            isEmpty(31)=False
             
-        elif vert == 2:
+        elif vert == 2 and isEmpty(32)=True:
             Sprite (Os,(380,230))
+            isEmpty(32)=False
             
-        elif vert == 3:
+        elif vert == 3 and isEmpty(33)=True:
             Sprite (Os,(380,360))
+            isEmpty(33)=False
 
 
 def mouseClick(event):                   #responds to clicks; the player's turn
