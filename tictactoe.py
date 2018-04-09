46a91d6111ed5190393bb0f817fdb9a30d7f6532
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-09 08:23:55 -0400

    Updated from Brython Server: 09/04/2018 08:23:53

--- tictactoe.py
+++ tictactoe.py
@@ -28,45 +28,45 @@ def computerTurn():             #picks a random unused square and places the com
     
     if horiz==1:                                        #decides for first column numbers
         
-        if vert == 1 and data[square11]==True:
+        if vert == 1 and data["square11"]==True:
             Sprite (Os,(120,100))
-            data[square11]= False
+            data["square11"]= False
             
-        elif vert == 2 and data[square12]==True:
+        elif vert == 2 and data["square12"]==True:
             Sprite (Os,(120,230))
-            data[square12]=False
+            data["square12"]=False
             
-        elif vert == 3 and data[square13]==True:
+        elif vert == 3 and data["square13"]==True:
             Sprite (Os,(120,360))
-            data[square13]=False
+            data["square13"]=False
             
     elif horiz == 2:                                    #decides for second column numbers
         
-        if vert == 1 and data[square21]==True:
+        if vert == 1 and data["square21"]==True:
             Sprite (Os,(250,100))
-            data[square21]=False
+            data["square21"]=False
             
-        elif vert == 2 and data[square22]==True:
+        elif vert == 2 and data["square22"]==True:
             Sprite (Os,(250,230))
-            data[square22]=False
+            data["square22"]=False
             
-        elif vert == 3 and data[square23]==True:
+        elif vert == 3 and data["square23"]==True:
             Sprite (Os,(250,360))
-            data[square23]=False
+            data["square23"]=False
             
     elif horiz == 3:                            #decides for third column numbers
         
-        if vert == 1 and data[square31]==True:
+        if vert == 1 and data["square31"]==True:
             Sprite (Os,(380,100))
-            data[square31]=False
+            data["square31"]=False
             
-        elif vert == 2 and data[square32]==True:
+        elif vert == 2 and data["square32"]==True:
             Sprite (Os,(380,230))
-            data[square32]=False
+            data["square32"]=False
             
-        elif vert == 3 and data[square33]==True:
+        elif vert == 3 and data["square33"]==True:
             Sprite (Os,(380,360))
-            data[square33]=False
+            data["square33"]=False
 
 
 def mouseClick(event):                   #responds to clicks; the player's turn
@@ -120,15 +120,15 @@ if __name__ == '__main__':
 
 
     data = {}
-    data[square11] = True
-    data[square12] = True
-    data[square13] = True
-    data[square21] = True
-    data[square22] = True
-    data[square23] = True
-    data[square31] = True
-    data[square32] = True
-    data[square33] = True
+    data["square11"] = True
+    data["square12"] = True
+    data["square13"] = True
+    data["square21"] = True
+    data["square22"] = True
+    data["square23"] = True
+    data["square31"] = True
+    data["square32"] = True
+    data["square33"] = True
 
 
     #graphics
