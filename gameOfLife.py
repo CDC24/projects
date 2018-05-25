61bada4473d73f739dded8c2337b1e30c625c000
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-05-25 08:30:21 -0400

    Updated from Brython Server: 25/05/2018 08:30:20

--- gameOfLife.py
+++ gameOfLife.py
@@ -20,6 +20,30 @@ def buildBoard():    #sprites a 13 x 25 matrix of zeroes(dead squares)
 def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
+    #creating graphics
+    
+    black = Color(0x000000,0.25)
+    red = Color(0xFF0000,1)
+    yellow = Color(0xFFFF00,0.25)
+    
+    #setting up board
+    
+    HEIGHT = 520
+    WIDTH = 1000
+    
+    
+    vertBoardLine = RectangleAsset(2,HEIGHT,LineStyle(0,black),black)
+    horizBoardLine = RectangleAsset(WIDTH,2,LineStyle(0,black),black)
+    boardBox = RectangleAsset(WIDTH,HEIGHT,LineStyle(1,yellow),yellow)
+    
+    
+    
+    # these sprite the grid
+    Sprite(boardBox)
+    for i in range (0,HEIGHT//40):              #sprites horizontal gridlines with 40 spacing
+        Sprite(horizBoardLine,(0,(40*i)))
+    for i in range (0,WIDTH//40):               #sprites vertical gridlines with 40 spacing
+        Sprite(vertBoardLine,((40*i),0))
     
 def numNeighbors(row,col): 
     return
@@ -37,8 +61,8 @@ def mouseClick(event):
 
 if __name__ == '__main__':
 
-
-    matrix = [buildBoard()]
+    data = {}
+    data["matrix"] = buildBoard()
 
 
     #graphics
