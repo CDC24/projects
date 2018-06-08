073744fa81b1e389cb22995daa4460a7e19b4856
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-06-08 08:04:35 -0400

    Updated from Brython Server: 08/06/2018 08:04:34

--- smallBoardGameOfLife.py
+++ smallBoardGameOfLife.py
@@ -22,15 +22,13 @@ def mouseClick(event):          #deals with mouse click coordinates and resultin
     row = (int(event.y//40))           #senses click in horizontal gridlines with 40 spacing
     col = (int(event.x//40))           #senses click in vertical gridlines with 40 spacing
     
-    print("row",row,"col",col)
-    
     if 0<= event.x <=400 and 0<=event.y<=400 and data["matrix"][row][col] == 0:
         Sprite(liveCell,(event.x-(event.x%40),event.y-(event.y%40)))
-        data["matrix"][row][col] = 1         #reverses status in matrix
+        data["matrix"][row][col] = 1         #reverses square status in matrix
 
     elif 0<= event.x <=400 and 0<=event.y<=400 and data["matrix"][row][col] == 1:
         Sprite(deadCell,(event.x-(event.x%40),event.y-(event.y%40)))
-        data["matrix"][row][col] = 0           #reverses status in matrix
+        data["matrix"][row][col] = 0           #reverses square status in matrix
         
     elif 0<= event.x <=100 and 420<=event.y<=480:          #nextGen button
         redrawAll()
